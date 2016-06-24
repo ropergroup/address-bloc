3b3913a7479c210dd583745bfefd51bd169aaa18
Christopher Roper <chris@roper.xyz>
2016-06-24 11:07:49 -0600

    Entry
    Entry

---
+++ entry.rb
@@ -0,0 +1,10 @@
+class Entry
+   attr_accessor :name, :phone_number, :email
+
+   def initialize(name, phone_number, email)
+      @name = name
+      @phone_number = name
+      @email = email
+   end
+
+ end
