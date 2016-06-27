b8bb1918be373ccf9df06552839d64768226eea1
Chris Roper <chris@roper.xyz>
2016-06-27 17:41:01 -0600

    CP21 fixed indexing issue in the check_entry CSV spec file

--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -59,25 +59,25 @@ RSpec.describe AddressBook do
      end
      it "imports the 2nd entry" do
        book.import_from_csv("entries.csv")
-       entry_two = book.entries[0]
+       entry_two = book.entries[1]
 
        check_entry(entry_two, "Bob", "555-555-5415", "bob@blocemail.com")
      end
    it "imports the 3rd entry" do
        book.import_from_csv("entries.csv")
-       entry_three = book.entries[0]
+       entry_three = book.entries[2]
 
-       check_entry(entry_three, "Joe", "555-555-Joe", "joe@blocemail.com")
+       check_entry(entry_three, "Joe", "555-555-3660", "joe@blocemail.com")
    end
    it "imports the 4th entry" do
        book.import_from_csv("entries.csv")
-       entry_four = book.entries[0]
+       entry_four = book.entries[3]
 
        check_entry(entry_four, "Sally", "555-555-4646", "sally@blocemail.com")
    end
    it "imports the 5th entry" do
        book.import_from_csv("entries.csv")
-       entry_five = book.entries[0]
+       entry_five = book.entries[4]
 
        check_entry(entry_five, "Sussie", "555-555-2036", "sussie@blocemail.com")
    end
