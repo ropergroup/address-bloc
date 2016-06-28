0f57411978124c3b95b0c3362fb59e4bd713b97c
Chris Roper <chris@roper.xyz>
2016-06-28 09:11:30 -0600

    add features to menu i.e. search, edit, delete

--- controllers/menu_controller.rb
+++ controllers/menu_controller.rb
@@ -98,6 +98,16 @@ def create_entry
 end
 
 def search_entries
+    print "Search by name: "
+    name = gets.chomp
+    match = address_book.binary_search(name)
+    system "clear"
+  if match
+    puts match.to_s
+    search_submenu(match)
+  else
+    puts "No match found for #{name}"
+  end
 end
 
 def read_csv
@@ -119,7 +129,7 @@ rescue
 end
 end
 
-def entry_submenu
+def entry_submenu(entry)
 
   puts "n - next entry"
   puts "d - delete entry"
@@ -131,9 +141,11 @@ def entry_submenu
   case selection
   when "n"
   when "d"
+    delete_entry(entry)
   when "e"
+    edit_entry(entry)
+    entry_submenu(entry)
   when "m"
-
     system "clear"
     main_menu
   else
@@ -141,5 +153,56 @@ def entry_submenu
     puts "#{selection} is not a valid input"
     entry_submenu(entry)
   end
+end
+
+def delete_entry(entry)
+  address_book.entries.delete(entry)
+  puts "#{entry.name} has been deleted"
+end
+
+def edit_entry(entry)
+  print "Updated name: "
+  name = gets.chomp
+  print "Updated phone number: "
+  phone_number = gets.chomp
+  print "Updated email: "
+  email = gets.chomp
+
+  entry.name = name if !name.empty?
+  entry.phone_number = phone_number if !phone_number.empty?
+  entry.email = email if !email.empty?
+  system "clear"
+
+  puts "Updated entry:"
+  puts entry
+end
+
+def search_submenu(entry)
+
+puts "\nd - delete entry"
+puts "e - edit this entry"
+puts "m - return to main menu"
+
+selection = gets.chomp
+
+case selection
+when "d"
+  system "clear"
+  delete_entry(entry)
+  main_menu
+when "e"
+  edit_entry(entry)
+  system "clear"
+  main_menu
+when "m"
+  system "clear"
+  main_menu
+else
+  system "clear"
+  puts "#{selection} is not a valid input"
+  puts entry.to_s
+  search_submenu(entry)
+end
+
 end
 end
